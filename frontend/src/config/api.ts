c57dcfba1359c520c32493519accdff46fc3ec49
Kamila <kamila.weronika.marcinek@gmail.com>
2024-06-28 00:20:23 +0200

    - logout when credentials expired

--- frontend/src/config/api.ts
+++ frontend/src/config/api.ts
@@ -17,17 +17,17 @@ const getToken = () => {
     return config;
   });
 
-  // api.interceptors.response.use(
-  //   (response) => {
-  //     return response;
-  //   },
-  //   (error) => {
-  //     if (error.response?.status === 403 && error.response.data.message === "Invalid token") {
-  //       localStorage.removeItem('authToken');
-  //       window.location.href = '/auth/login';
-  //     }
-  //     return Promise.reject(error);
-  //   }
-  // );
+  api.interceptors.response.use(
+    (response) => {
+      return response;
+    },
+    (error) => {
+      if (error.response?.status === 403 && error.response.data.message === "Invalid token") {
+        localStorage.removeItem('authToken');
+        window.location.href = '/auth/login';
+      }
+      return Promise.reject(error);
+    }
+  );
 
 export default api;
